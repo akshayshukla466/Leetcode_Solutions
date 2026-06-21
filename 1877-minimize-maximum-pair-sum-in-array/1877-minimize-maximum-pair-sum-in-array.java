fd3a7b908d990d83e02e7cfa73d8a7eaa0880143
Akshay Shukla <123648369+akshayshukla466@users.noreply.github.com>
2026-06-21 20:23:25 +0530

    1877-minimize-maximum-pair-sum-in-array

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
+        int ans = 0;
+
+        while (i < j) {
+            ans = Math.max(ans, nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
