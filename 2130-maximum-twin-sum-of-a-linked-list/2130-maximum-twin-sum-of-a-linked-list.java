d3249ed81675bb845cdeb2378188030e51ecbc9b
Akshay Shukla <123648369+akshayshukla466@users.noreply.github.com>
2026-06-22 18:51:03 +0530

    2130-maximum-twin-sum-of-a-linked-list

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int pairSum(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+
+        while(slow != null){
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Compare 
+        int maxSum = 0;
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while(second != null){
+            maxSum = Math.max(maxSum,
+                    first.val + second.val);
+
+            first = first.next;
+            second = second.next;
+        }
+
+        return maxSum;
+    }
+}
