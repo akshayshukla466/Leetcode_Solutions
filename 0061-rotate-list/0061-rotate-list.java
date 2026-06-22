1eddd7b82782a559ddd359c403f1b8a1860b3335
Akshay Shukla <123648369+akshayshukla466@users.noreply.github.com>
2026-06-22 19:04:19 +0530

    0061-rotate-list

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,38 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode tail = head;
+        int length = 1;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        
+        tail.next = head;
+
+     
+        k = k % length;
+
+        int steps = length - k;
+
+        ListNode newTail = tail;
+
+        while (steps > 0) {
+            newTail = newTail.next;
+            steps--;
+        }
+
+        
+        ListNode newHead = newTail.next;
+
+        newTail.next = null;
+
+        return newHead;
+    }
+}
