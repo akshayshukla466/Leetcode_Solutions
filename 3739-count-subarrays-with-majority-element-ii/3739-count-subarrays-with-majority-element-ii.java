720e9bc986e546ed98453e34a51c98fef45255c0
Akshay Shukla <123648369+akshayshukla466@users.noreply.github.com>
2026-06-26 08:18:27 +0530

    3739-count-subarrays-with-majority-element-ii

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    class Fenwick {
+        int[] bit;
+
+        Fenwick(int n) {
+            bit = new int[n + 2];
+        }
+
+        void update(int idx, int val) {
+            while (idx < bit.length) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        int[] sorted = prefix.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int id = 1;
+
+        for (int x : sorted) {
+            if (!map.containsKey(x)) {
+                map.put(x, id++);
+            }
+        }
+
+        Fenwick bit = new Fenwick(id);
+
+        long ans = 0;
+
+        for (int x : prefix) {
+            int idx = map.get(x);
+            ans += bit.query(idx - 1);
+            bit.update(idx, 1);
+        }
+
+        return ans;
+    }
+}
